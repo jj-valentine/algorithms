1887f9e22992089d49421a7bbb4af96ca77bb2fa
JD <j@mesjohnson.com>
2018-07-07 22:13:42 -0700

    Completed 'reverse-in-place'

---
+++ problems/reverse-in-place.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/*
+Write a function to reverse an array in place, where "in place" means "without creating a new object in memory".
+
+  In some languages, strings are mutable (like in Ruby). In other languages,
+  such as Python and JavaScript, strings are immutable, meaning they CANNOT
+  be changed after they're created.
+
+  Since strings are immutable in JS, we will be reversing an array of characters instead.
+
+DO NOT USE THE BUILT IN REVERSE METHOD!
+*/
+
+/*
+n = # of characters in input array
++ RUNTIME Complexity: O(n/2) -> O(n)
++ SPACE Complexity: O(1) [BST/WST]
+NOTE:
+*/
+
+const reverseInPlace = arr => {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+  return arr;
+}
+
+const array = ['a', 'b', 'c', 'd', 'e'];
+console.log(reverseInPlace(array)); // -> ['e', 'd', 'c', 'b', 'a']
