562eaa75ff01d8b88216d55745e43156aad6cae7
James Valentine <j@mesvalentine.com>
2022-03-08 23:06:37 -0800

    TypeScript Implementations for 'DoublyLinkedList' and Associated 'DLLNode' Class

---
+++ data-structures/DLLNode.ts
@@ -0,0 +1,21 @@
+export class DLLNode<V> {
+  // private _key: K; 
+  private _value: V;
+  public next: DLLNode<V> | null;
+  public prev: DLLNode<V> | null;
+
+  constructor(value: V, prev?: DLLNode<V> | null, next?: DLLNode<V> | null) {
+    // this._key = key;
+    this._value = value;
+    this.prev = prev || null;
+    this.next = next || null;
+  }
+  
+  // get key(): K {
+  //   return this._key;
+  // }
+
+  get value(): V {
+    return this._value;
+  }
+}
---
+++ data-structures/DoublyLinkedList.ts
@@ -0,0 +1,102 @@
+import { DLLNode } from "./DLLNode";
+
+class DoublyLinkedList<V> {
+  public head: DLLNode<V> | null;
+  public tail: DLLNode<V> | null;
+  private size: number;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  public get length(): number {
+    return this.size;
+  }
+
+  public isEmpty(): boolean {
+    return this.size === 0;
+  }
+
+  // public update(id: string, updatedValue: V): DLLNode<V> | null {
+  //   let nextNode = null;
+  //   if (id && id.length) nextNode = node.next
+  //   const updatedNode = this.insertBefore(updatedValue, nextNode);    
+  //   return updatedNode ? updatedNode : null;
+  // }
+
+  public insertBefore(value: V, nextNode?: DLLNode<V> | null): DLLNode<V> | undefined {
+    if (value === null || value === undefined) return;
+    let node = undefined;
+    if (!nextNode && this.isEmpty()) { // list is empty → new node is assigned to both 'head' and 'tail'
+      node = new DLLNode(value);
+      this.head = node;
+      this.tail = this.head;
+    } else if (!this.isEmpty()) { // list contains at least one node
+      if (!nextNode && this.tail) { // insert new node at end of (non-empty) list
+        const prevNode = this.tail;
+        node = new DLLNode(value, prevNode, null);
+        prevNode.next = node;
+        this.tail = node;
+      } else if (nextNode && nextNode === this.head) { // insert new node at beginning of (non-empty) list
+        const nextNode = this.head;
+        node = new DLLNode(value, null, nextNode);
+        nextNode.prev = node;
+        this.head = node;
+      } else if (nextNode) { // insert new node somewhere in the middle of list
+        const prevNode = nextNode.prev;
+        node = new DLLNode(value, prevNode, nextNode);
+        nextNode.prev = node;
+        if (prevNode) prevNode.next = node;
+      }
+    }
+
+    this.size++;
+    return node;
+  }
+  
+  public remove(node: DLLNode<V>): DLLNode<V> {
+    if (!node.prev && !node.next && this.head === node && this.tail === node) { // remove only node contained in list
+      this.head = null;
+      this.tail = null;
+    } else if (!node.prev && node.next && this.head === node) { // remove node from beginning of list (containing at least one other node)
+      const nextNode = node.next; 
+      this.head = nextNode;
+      node.next = null;
+      nextNode.prev = null;
+    } else if (!node.next && node.prev && this.tail === node) { // remove node from end of list (containing at least one other node)
+      const prevNode = node.prev;
+      this.tail = prevNode;
+      node.prev = null;
+      prevNode.next = null;
+    } else if (node.prev && node.next) { // remove node from middle of the list
+      const prevNode = node.prev, nextNode = node.next;
+      prevNode.next = nextNode;
+      nextNode.prev = prevNode;
+      node.prev = null;
+      node.next = null;
+    } 
+
+    this.size--;
+    return node;
+  }
+
+  public shift(node: DLLNode<V>, moveTowardHead: boolean): void {
+    if (this.size < 2) return;
+    const prevNode = node.prev, nextNode = node.next;
+    this.remove(node);
+
+    if (moveTowardHead && prevNode) {
+      this.insertBefore(node.value, prevNode);
+    } else if (!moveTowardHead && nextNode) {
+      if (nextNode.next) this.insertBefore(node.value, nextNode.next);
+      else this.insertBefore(node.value)
+    }
+  }
+}
+
+export {
+  DLLNode,
+  DoublyLinkedList
+};
